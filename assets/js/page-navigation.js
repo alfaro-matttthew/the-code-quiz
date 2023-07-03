074d6eeb32ca6e538a129875338484b83ccd12ae
alfaro-matttthew <137105768+alfaro-matttthew@users.noreply.github.com>
2023-07-03 16:05:44 -0400

    added page naviagation, as well as the beginnings of the question generation functions

---
+++ assets/js/page-navigation.js
@@ -0,0 +1,33 @@
+function homeActive() {
+    document.getElementById("homeScreen").style.display = "flex";
+
+    document.getElementById("rulesScreen").style.display = "none";
+    document.getElementById("gameScreen").style.display = "none";
+    document.getElementById("scoreScreen").style.display = "none";
+}
+
+function rulesActive() {
+    document.getElementById("rulesScreen").style.display = "flex";
+
+    document.getElementById("homeScreen").style.display = "none";
+    document.getElementById("gameScreen").style.display = "none";
+    document.getElementById("scoreScreen").style.display = "none";
+}
+
+function gameActive() {
+    document.getElementById("gameScreen").style.display = "flex";
+
+    document.getElementById("homeScreen").style.display = "none";
+    document.getElementById("rulesScreen").style.display = "none";
+    document.getElementById("scoreScreen").style.display = "none";
+}
+
+function scoreActive() {
+    document.getElementById("scoreScreen").style.display = "flex";
+
+    document.getElementById("homeScreen").style.display = "none";
+    document.getElementById("rulesScreen").style.display = "none";
+    document.getElementById("gameScreen").style.display = "none";
+}
+
+document.getElementById("startTest").addEventListener('click', gameActive)
