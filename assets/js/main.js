4b3d5fd504aa3c8d960e18ac04ac0c015ebec33a
alfaro-matttthew <137105768+alfaro-matttthew@users.noreply.github.com>
2023-07-05 16:38:46 -0400

    completed the question and snwers genereation funciton

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,57 +2,36 @@ var score = 0;
 
 var currentQuestion = 0;
 
-var radioInputs = document.querySelectorAll("radio-option");
-console.log(radioInputs);
+var radioA = document.getElementById("optA");
+var radioB = document.getElementById("optB");
+var radioC = document.getElementById("optC");
+var radioD = document.getElementById("optD");
+
+let radioOptions = [radioA, radioB, radioC, radioD]
 
 function questionSwitch() {
 
-    var questionContainer = document.getElementById("questionBox");
+    // Variables and Arrays compiling data for the Functions
+    var questionContainer = document.getElementById("questionItem");
 
+    questionContainer.textContent = questionList[0].question;
 
-/* This is where I connected each <p> to the respective var
-    // var optionA = document.getElementById("optA");
-    // var optionB = document.getElementById("optB");
-    // var optionC = document.getElementById("optC");
-    // var optionD = document.getElementById("optD");
-*/
-
-    questionContainer.textContent = questionList[currentQuestion].question;
-
-    questionContainer.innerHTML = "",
-    questionList[currentQuestion].options.forEach(option => {
-        // choice
-    })
-
-
-
-
-
-    question.textContent = questionList[0].question;
-    optionA.textContent = questionList[0].options[0];
-    optionB.textContent = questionList[0].options[1];
-    optionC.textContent = questionList[0].options[2];
-    optionD.textContent = qquestionList[0].options[3];
+    for (var i = 0; i < radioOptions.length; i++) {
+        radioOptions[i].textContent = questionList[0].options[i];
+        console.log(radioOptions[i].textContent);
+    }
+    
+    
 }
 
-// function checkAnswer() {
-//         var questionForm = document.querySelector("input[name=option]:checked")
-//         console.log(questionForm);
-//         console.dir(questionForm);
-//         answer = qOne.answer;
-//         console.log(answer);
-//     if (questionForm === answer) {
-//         alert("This is working");
-//         console.log(score);
-//     }
-//     else if (questionForm != answer) {
-//         alert("This is NOT working");
-//         console.log(score);
-//     }
-// }
+document.getElementById("startTest").addEventListener("click", questionSwitch);
 
+function init() {
+    clear
+    document.getElementById("questionItem").textContent = "";
+    
+    for (var i = 0; i < radioOptions.length; i++) {
+        radioOptions[i].textContent = "";
+    }
 
-
-document.getElementById("startTest").addEventListener('click', questionSwitch)
-
-document.getElementById("submitBtn").addEventListener("click", checkAnswer);
+}
